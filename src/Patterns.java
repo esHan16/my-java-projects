92c42a4c486fdfb3600123b407706edf87e9eaec
Eshan <v.eshan16@gmail.com>
2023-08-13 23:52:06 +0530

    Patterns

--- src/Patterns.java
+++ src/Patterns.java
@@ -4,7 +4,7 @@ public class Patterns {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        pattern5(n);
+        pattern6(n);
     }
     /*
      * * * * *
@@ -88,6 +88,28 @@ public class Patterns {
                 } else {
                     System.out.print("  ");
                 }
+            }
+            System.out.println();
+        }
+    }
+    /*
+     *                 *
+     * *             * *
+     * * *         * * *
+     * * * *     * * * *
+     * * * * * * * * * *
+     */
+    public static void pattern6(int n){
+        for(int i = 1 ; i <= n ; i++){
+
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print("* ");
+            }
+            for(int k = 1 ; k <= 2 * (n - i) ; k++){
+                System.out.print("  ");
+            }
+            for(int j = 0 ; j < i ; j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
